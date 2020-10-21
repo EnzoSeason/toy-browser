272ce68d1911ef7627aa4858190fa0b15e2a6e64
Jijie LIU <liujijieseason@gmail.com>
2020-10-21 11:00:39 +0200

    Update trunked-body-parser.js

--- src/tools/trunked-body-parser.js
+++ src/tools/trunked-body-parser.js
@@ -1,46 +1,46 @@
 class TrunkedBodyParser {
 	constructor() {
-		this.LENGTH = 0
-		this.LENGTH_END = 1
-		this.READING_TRUNK = 2
-		this.NEW_LINE = 3
-		this.NEW_LINE_END = 4
+		this.LENGTH = 0;
+		this.LENGTH_END = 1;
+		this.READING_TRUNK = 2;
+		this.NEW_LINE = 3;
+		this.NEW_LINE_END = 4;
 
-		this.current = this.LENGTH
-		this.length = 0
-		this.content = []
-		this.isFinished = false
+		this.current = this.LENGTH;
+		this.length = 0;
+		this.content = [];
+		this.isFinished = false;
 	}
 	receiveChar(char) {
 		if (this.current === this.LENGTH) {
 			if (char === '\r') {
 				if (this.length === 0) {
-					this.isFinished = true
-					this.current = this.NEW_LINE
+					this.isFinished = true;
+					this.current = this.NEW_LINE;
 				} else {
-					this.current = this.LENGTH_END
+					this.current = this.LENGTH_END;
 				}
 			} else {
-				this.length *= 16
-				this.length += parseInt(char, 16)
+				this.length *= 16;
+				this.length += parseInt(char, 16);
 			}
 		} else if (this.current === this.LENGTH_END) {
 			if (char === '\n') {
-				this.current = this.READING_TRUNK
+				this.current = this.READING_TRUNK;
 			}
 		} else if (this.current === this.READING_TRUNK) {
-			this.content.push(char)
-			this.length--
+			this.content.push(char);
+			this.length--;
 			if (this.length === 0) {
-				this.current = this.NEW_LINE
+				this.current = this.NEW_LINE;
 			}
 		} else if (this.current === this.NEW_LINE) {
 			if (char === '\r') {
-				this.current = this.NEW_LINE_END
+				this.current = this.NEW_LINE_END;
 			}
 		} else if (this.current === this.NEW_LINE_END) {
 			if (char === '\n') {
-				this.current = this.LENGTH
+				this.current = this.LENGTH;
 			}
 		}
 	}
